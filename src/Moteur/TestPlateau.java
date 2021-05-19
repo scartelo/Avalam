4c2db5a4aec5790af446f8d2d5bf5f15b3961d95
scartelo <scartelo@icloud.com>
2021-05-19 13:03:33 +0200

    Dossier src

---
+++ src/Moteur/TestPlateau.java
@@ -0,0 +1,17 @@
+package Moteur;
+
+public class TestPlateau {
+    public static void main(String[] args) {
+        PlateauDeJeu p  = new PlateauDeJeu();
+        p.afficher_grille();
+        p.tour(3,5).ajouteTour(p.tour(3,4));
+        p.afficher_grille();
+        p.tour(3,5).ajouteTour(p.tour(2,5));
+        p.afficher_grille();
+        p.tour(3,5).ajouteTour(p.tour(3,6));
+        p.afficher_grille();
+        p.tour(3,5).ajouteTour(p.tour(4,5));
+        p.afficher_grille();
+        System.out.println(p.tour(3,5).estDeplacable(p.tour(2,5)));
+    }
+}
