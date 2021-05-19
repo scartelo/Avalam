b7f068f158accfbc0eea5c0dbd01ee680e6b2a65
Yann <hoyer.yann@gmail.com>
2021-05-19 16:04:10 +0200

    Début sauvegardes ( peut sauvegarder mais pas lire une sauvegarde )

--- src/Moteur/PlateauDeJeu.java
+++ src/Moteur/PlateauDeJeu.java
@@ -193,6 +193,10 @@ public class PlateauDeJeu extends Historique<Coup>{
     public Tour tour(int l, int c){
         return grille[l][c];
     }
+
+    public void Jouer_pos(int i_1, int j_1, int i_2, int j_2){
+        Jouer(grille[i_1][j_1],grille[i_2][j_2]);
+    }
     public void Jouer(Tour src, Tour dst){
         Tour tmp_src,tmp_dst;
         tmp_src=new Tour(src.contenu(),src.ligne,src.colonne);
@@ -219,5 +223,9 @@ public class PlateauDeJeu extends Historique<Coup>{
         else{
             System.out.println("Ne peut pas annuler le coup");
         }
+    }
+    public void sauvegarder(){
+        Saves S = new Saves();
+        S.write_save(passe,futur);
     }
 }
