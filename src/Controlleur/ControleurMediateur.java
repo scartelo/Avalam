13a71ac9cc036598be1f4e0bcf50b134aa4e5f13
benoit <benoit.decachard@wanadoo.fr>
2021-05-19 17:53:16 +0200

    Ajout interfaces

--- src/Controlleur/ControleurMediateur.java
+++ src/Controlleur/ControleurMediateur.java
@@ -1,4 +1,28 @@
 package Controlleur;
 
-public class ControleurMediateur {
+import Moteur.Coup;
+import Vue.CollecteurEvenements;
+import Vue.InterfaceUtilisateur;
+
+public class ControleurMediateur implements CollecteurEvenements {
+
+    @Override
+    public void joue(int l, int c) {
+
+    }
+
+    @Override
+    public void clicSouris(int l, int c) {
+
+    }
+
+    @Override
+    public void jouerCoup(Coup coup) {
+
+    }
+
+    @Override
+    public void fixerIU(InterfaceUtilisateur iu) {
+
+    }
 }
