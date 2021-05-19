891a25e9a39ac116dddc4904e23a714309fccca9
scartelo <58155534+scartelo@users.noreply.github.com>
2021-05-19 13:14:30 +0200

    Delete Structures directory

--- Structures/Sequence.java
+++
@@ -1,9 +0,0 @@
-package Structures;
-
-public interface Sequence<Toto> {
-	void insereTete(Toto element);
-	void insereQueue(Toto element);
-	Toto extraitTete();
-	boolean estVide();
-	Iterateur<Toto> iterateur();
-}
