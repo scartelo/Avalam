952ae9f4a6423e86587e94eb68628093344a492b
Mehrnaz <58178612+YAGHOUBM@users.noreply.github.com>
2021-05-19 15:31:23 +0200

    Add files via upload
    Les joueurs

---
+++ src/Controlleur/JoueurIA.java
@@ -0,0 +1,14 @@
+package Moteur;
+
+public class JoueurIA extends Joueur{
+    JoueurIA(int n, PlateauDeJeu pj){
+        super(n,pj);
+    }
+    @Override
+    boolean tempsEcoule(){
+        int i,j;
+
+        return true;
+    }
+
+}
